7e4a69be65247e896560b62f2b22035fbe073b16
veyselboybay <veysel.boybay@outlook.com>
2022-12-05 20:48:00 -0500

    Profile change update

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import styled from "styled-components";
 
 const User = {
@@ -11,6 +17,7 @@ const User = {
     model: "Civic",
     year: "2015",
     color: "blue",
+    transmission: "auto",
     engine: "1.8",
     fuelType: "gas",
   },
@@ -18,37 +25,69 @@ const User = {
 };
 
 function Profile() {
+  const [active, setActive] = useState("personal");
   return (
-    <>
-      <Section>
-        <h5>Personal information:</h5>
-        <p>Full Name: {User.firstName + " " + User.lastName}</p>
-        <p>Phone Number: {User.phoneNumber}</p>
-        <p>Address: {User.address}</p>
-      </Section>
-      <Section>
-        <h5>Car Details:</h5>
-        <p>Year: {User.car.year}</p>
-        <p>Make: {User.car.make}</p>
-        <p>Model: {User.car.model}</p>
-        <p>Color: {User.car.color}</p>
-        <p>Engine: {User.car.engine}</p>
-        <p>Fuel Type: {User.car.fuelType}</p>
-      </Section>
-      <Section>
-        <h5>Appointment History:</h5>
-        {User.appointments.length === 0 && <p>No Appointment history made!</p>}
-      </Section>
-    </>
+    <Container style={{ marginTop: "20px" }}>
+      <Row>
+        <Col className="d-grid gap-2" md={4}>
+          <Button variant="info" onClick={() => setActive("personal")}>
+            Personal Information
+          </Button>
+          <Button variant="info" onClick={() => setActive("carDetail")}>
+            Card Details
+          </Button>
+          <Button
+            variant="info"
+            onClick={() => setActive("appointmentHistory")}
+          >
+            Appointment History
+          </Button>
+        </Col>
+        <Col>
+          {active === "personal" && (
+            <Card>
+              <Section>
+                <h5>Personal information:</h5>
+                <p>Full Name: {User.firstName + " " + User.lastName}</p>
+                <p>Phone Number: {User.phoneNumber}</p>
+                <p>Address: {User.address}</p>{" "}
+              </Section>
+            </Card>
+          )}
+          {active === "carDetail" && (
+            <Card>
+              <Section>
+                <h5>Car Details:</h5>
+                <p>Year: {User.car.year}</p>
+                <p>Make: {User.car.make}</p>
+                <p>Model: {User.car.model}</p>
+                <p>Color: {User.car.color}</p>
+                <p>Transmission: {User.car.transmission}</p>
+                <p>Engine: {User.car.engine}</p>
+                <p>Fuel Type: {User.car.fuelType}</p>{" "}
+              </Section>
+            </Card>
+          )}
+          {active === "appointmentHistory" && (
+            <Card>
+              <Section>
+                <h5>Appointment History:</h5>{" "}
+                {User.appointments.length === 0 && (
+                  <p>No Appointment history made!</p>
+                )}{" "}
+              </Section>
+            </Card>
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 const Section = styled.div`
-  background-color: #eeeeee;
   width: 90%;
   margin-top: 10px;
   margin-left: auto;
   margin-right: auto;
   padding-left: 15px;
 `;
-
 export default Profile;
