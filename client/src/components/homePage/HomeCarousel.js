22f0cd293c36a54b804a2b4575c0ac2a8e7a88c4
veyselboybay <veysel.boybay@outlook.com>
2022-12-11 16:43:10 -0500

    general bug fix

--- client/src/components/homePage/HomeCarousel.js
+++ client/src/components/homePage/HomeCarousel.js
@@ -11,48 +11,48 @@ function HomeCarousel() {
     <Carousel fade>
       <Carousel.Item>
         <img className="d-block w-100" src={mechanic} alt="First slide" />
-        <Carousel.Caption>
+        {/* <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+        </Carousel.Caption> */}
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={mechanic2} alt="Second slide" />
 
-        <Carousel.Caption>
+        {/* <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+        </Carousel.Caption> */}
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={mechanic3} alt="Third slide" />
 
-        <Carousel.Caption>
+        {/* <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
-        </Carousel.Caption>
+        </Carousel.Caption> */}
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={mechanic4} alt="Third slide" />
 
-        <Carousel.Caption>
+        {/* <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
-        </Carousel.Caption>
+        </Carousel.Caption> */}
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={mechanic5} alt="Third slide" />
 
-        <Carousel.Caption>
+        {/* <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
-        </Carousel.Caption>
+        </Carousel.Caption> */}
       </Carousel.Item>
     </Carousel>
   );
